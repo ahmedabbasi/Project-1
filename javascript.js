f75aeb8bc27932038869db1b7a74db81253407c9
Ahmed Abbasi <aabbasi@ncstech.com>
2018-03-16 00:49:52 -0400

    new change

--- javascript.js
+++ javascript.js
@@ -1,21 +1,7 @@
-var channelName= "IrfanJunejo"
+var channelName= "Ed Sheeran"
 
 $(document).ready (function(){
-    function pid(){
-        var queryURL="https://www.googleapis.com/youtube/v3/channels?part=contentDetails&forUsername="+channelName+"&key=AIzaSyC30mnkcq5ly-Nr-kHMh0f05WwzzVtuO3Q"
-
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-          }).then(function(response) {
-            console.log(response);
-
-
-
-          })
-    }
-      
-    function vid(){
+        function vid(){
         var queryURL="https://www.googleapis.com/youtube/v3/search?part=snippet&q="+channelName+"&key=AIzaSyC30mnkcq5ly-Nr-kHMh0f05WwzzVtuO3Q&maxResults=3"
 
 
