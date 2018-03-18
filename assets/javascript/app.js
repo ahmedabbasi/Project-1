ee11608bbfc31547f9dc5faab189143578d867e6
Ahmed Abbasi <aabbasi@ncstech.com>
2018-03-18 01:09:19 -0400

    new

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -18,9 +18,9 @@ $(document).ready (function(){
         console.log(response);
         var result= response;
         var vidId1=result.items[4].id.videoId;
-        var object1='<iframe width=\"100%\" height=\"100%\" src =\"http://www.youtube.com/embed/' +vidId1+ '\"></iframe>';
+        var object1='<iframe width=\"100%\" height=\"100%\" src =\"https://www.youtube.com/embed/' +vidId1+ '\"></iframe>';
         var vidId2=result.items[5].id.videoId
-        var object2='<iframe width=\"100%\" height=\"100%\"  src =\"http://www.youtube.com/embed/' +vidId2+ '\"></iframe>';
+        var object2='<iframe width=\"100%\" height=\"100%\"  src =\"https://www.youtube.com/embed/' +vidId2+ '\"></iframe>';
         $("#results1").append(object1);
         $("#results2").append(object2);
       })
